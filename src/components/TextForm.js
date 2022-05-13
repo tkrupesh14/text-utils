3bb28a8bdda465d0e8540c5a1403eaebd0bfceaf
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-05-13 10:02:07 +0530

    final version

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react'
 export default function TextForm(props) {
+
+
+
     const handlUpClick = () =>{
         let newText = text.toUpperCase();
         setText(newText)
@@ -25,17 +28,17 @@ export default function TextForm(props) {
 <div className="mb-3">
   <textarea className="form-control" id="myBox" onChange = {handleOnChange} rows="8" value = {text} style = {{backgroundColor:props.mode=== 'dark'? '#000a29':'white', color:props.mode=== 'dark'? 'white':'#000a29'}}></textarea>
 </div>
-<button className="btn btn-primary mx-2" onClick = {handlUpClick}>Convert To Uppercase</button>
-<button className="btn btn-primary mx-2" onClick = {handlLowClick}>Convert To Lowercase</button>
-<button className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy Text</button>
+<button className="btn btn-primary mx-2 my-1" disabled = {text.length===0} onClick = {handlUpClick}>Convert To Uppercase</button>
+<button className="btn btn-primary mx-2 my-1" disabled = {text.length===0} onClick = {handlLowClick}>Convert To Lowercase</button>
+<button className="btn btn-primary mx-2 my-1" disabled = {text.length===0} onClick={handleCopyClick}>Copy Text</button>
     </div>
 
     <div className="container my-4" style = {{color:props.mode=== 'dark'? 'white':'#000a29'}}>
         <h1>Your Text Summery</h1>
-        <p>{text.split(" ").length} words, {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} minutes to read</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words, {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read</p>
         <h3>Preview</h3>
-        <p>{text.length>0 ? text : 'Enter someting to preview here'}</p>
+        <p>{text.length>0 ? text : 'nothing to preview'}</p>
     </div>
     </>
   )
