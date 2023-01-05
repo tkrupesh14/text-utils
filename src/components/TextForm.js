c16491982cf389c0ac55c044234047ab32883101
tkrupesh14 <info.tkrupesh@gmail.com>
2023-01-05 17:56:14 +0530

    build

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,7 +37,7 @@ export default function TextForm(props) {
         <h1>Your Text Summery</h1>
         <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words, {text.length} characters</p>
         <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read</p>
-        <h3>Preview</h3>
+        <h3>Previeww</h3>
         <p>{text.length>0 ? text : 'nothing to preview'}</p>
     </div>
     </>
